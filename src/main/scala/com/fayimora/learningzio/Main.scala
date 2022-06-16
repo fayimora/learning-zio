0db1337cfdbca8f7cc5511a5053362b6625dc1d8
Fayi Femi-Balogun <fayi@fayimora.com>
2022-06-16 11:01:51 +0100

    Generate initial project

---
+++ src/main/scala/com/fayimora/learningzio/Main.scala
@@ -0,0 +1,9 @@
+package com.fayimora
+package learningzio
+
+@main def Main(args: String*): Unit =
+  println("─" * 100)
+
+  println("hello world")
+
+  println("─" * 100)
---
+++ src/test/scala/com/fayimora/learningzio/ExampleSuite.scala
@@ -0,0 +1,7 @@
+package com.fayimora
+package learningzio
+
+final class ExampleSuite extends TestSuite:
+  test("hello world") {
+    1 `shouldBe` 1
+  }
