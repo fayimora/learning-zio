e37497f911ef5ff11d22f89a5586247430a9d3e2
Fayi Femi-Balogun <fayi@fayimora.com>
2022-06-18 12:31:49 +0100

    Add store selector

---
+++ src/main/scala/com/fayimora/learningzio/storeselector/StoreSelector.scala
@@ -0,0 +1,17 @@
+package com.fayimora.learningzio.storeselector
+
+import zio.*
+
+object StoreSelector:
+  def apply[T](tasks: List[Task[T]], delay: Duration): ZIO[Any, Throwable, T] =
+    tasks match
+      case Nil => ZIO.fail(new IllegalArgumentException("No stores available"))
+      case task :: Nil => task
+      case task :: remainingTasks =>
+        // use single element queue to know if there was a failure
+        Queue.bounded[Unit](1).flatMap { taskFailedQueue =>
+          val taskWithSignalFailed = task.onError(_ => taskFailedQueue.offer(()))
+          // we either hold for ack or skip that if the request failed
+          val sleepOrFailed = ZIO.sleep(delay).race(taskFailedQueue.take)
+          taskWithSignalFailed.race(sleepOrFailed *> apply(remainingTasks, delay))
+        }
